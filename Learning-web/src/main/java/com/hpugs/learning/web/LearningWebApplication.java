aef3c924273a0491663d2d18334cd6de631c1c3c
hpugs <1453296946@qq.com>
2020-11-11 17:00:06 +0800

    配置springboot不默认配置数据源

--- Learning-web/src/main/java/com/hpugs/learning/web/LearningWebApplication.java
+++ Learning-web/src/main/java/com/hpugs/learning/web/LearningWebApplication.java
@@ -3,13 +3,14 @@ package com.hpugs.learning.web;
 import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @DubboComponentScan
 public class LearningWebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LearningWebApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LearningWebApplication.class, args);
+    }
 
 }
